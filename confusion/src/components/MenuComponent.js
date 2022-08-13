fba2d67ce285f44268c0665d912c2496c1395c64
Emily <emily.vanessavilla@hotmail.com>
2022-08-13 16:38:58 -0500

    Components Part 1

---
+++ confusion/src/components/MenuComponent.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+
+class Menu extends Component {
+
+    //Constructor de utileria
+    constructor(props) {
+        //Suministra los accesorios (props) a la super clase
+        super(props);
+
+        this.state = {
+            dishes: [
+                {
+                  id: 0,
+                  name:'Uthappizza',
+                  image: 'assets/images/uthappizza.png',
+                  category: 'mains',
+                  label:'Hot',
+                  price:'4.99',
+                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
+               {
+                  id: 1,
+                  name:'Zucchipakoda',
+                  image: 'assets/images/zucchipakoda.png',
+                  category: 'appetizer',
+                  label:'',
+                  price:'1.99',
+                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
+               {
+                  id: 2,
+                  name:'Vadonut',
+                  image: 'assets/images/vadonut.png',
+                  category: 'appetizer',
+                  label:'New',
+                  price:'1.99',
+                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
+               {
+                  id: 3,
+                  name:'ElaiCheese Cake',
+                  image: 'assets/images/elaicheesecake.png',
+                  category: 'dessert',
+                  label:'',
+                  price:'2.99',
+                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
+               ],
+        };
+    }
+
+    //Gira la vista correspondiente para este componente
+    render() {
+
+        const menu = this.state.dishes.map((dish) => {
+            return (
+                //Los elementos requieren de un atributo clave (key)
+                <div key={dish.id} className="col-12 mt-5">
+                    <Media tag="li">
+                        <Media left middle>
+                            <Media object src={dish.image} alt={dish.name} />
+                        </Media>
+                        <Media body className="ml-5">
+                            <Media heading>{dish.name}</Media>
+                            <p>{dish.description}</p>
+                        </Media>
+                    </Media>
+                </div>
+            );
+        });
+
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <Media list>
+                        {menu}
+                    </Media>
+                </div>
+            </div>
+        );
+    }
+}
+
+/*
+Exportar componente desde este archivo
+Necesario para importar este componente en cualquier lugar de la app
+*/
+export default Menu;
